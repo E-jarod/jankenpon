4b93aa4d1e2f50380d3d9dec61f4c44e91636f3d
E-jarod <ejilane.jarod@gmail.com>
2021-02-06 22:17:59 +0100

    backup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,3 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
-</template>
-
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
@@ -15,4 +8,17 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   }
 })
 export default class Home extends Vue {}
+/* 
+Jan Ken Pon Erreur,
+Mister-e-vent outils (debug vs code, cypress, ng prime + tailwind, formly)
+9h-10h: Jan Ken Pon
+10h-11h: Debug Vs code
+*/
 </script>
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Hello" />
+  </div>
+</template>
